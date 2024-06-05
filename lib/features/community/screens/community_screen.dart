a43035a88cff164e5e61e2f479d2c0f53d71718d
Mohammed Fahiz <138813355+Mohammed-Fahiz@users.noreply.github.com>
2024-06-05 21:39:36 +0530

    Update community_screen.dart

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' ;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/common/error_text.dart';
 import 'package:reddit_clone/core/common/loader.dart';
